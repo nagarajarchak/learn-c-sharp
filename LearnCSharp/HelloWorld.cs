7e7d369499e8498efdaeaedb1d54e72c0fc544d8
nagarajarchak <archak.nagaraj@gmail.com>
2023-07-05 12:41:49 +1000

    Added a hello world program and console output

---
+++ LearnCSharp/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LearnCSharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Output output = new Output();
+            
+        }
+    }
+}
---
+++ LearnCSharp/Output.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LearnCSharp
+{
+	public class Output
+	{
+		public Output()
+		{
+			// Single line comment
+			Console.WriteLine("This is a new line!");
+			Console.WriteLine(1 + 1);
+            Console.WriteLine("This is another line!");
+
+			/*
+			 * This is a multi-line comment
+			 */
+			Console.Write("This is on a new line!");
+            Console.Write("This is on the same line again!");
+        }
+	}
+}
+
